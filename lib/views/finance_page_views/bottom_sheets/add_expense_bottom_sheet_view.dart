01e9d49f6255f96507bd7af5ba4f6475d2188f0c
Jsimpson123 <jsimpson3489@gmail.com>
2025-01-20 18:17:31 +0000

    expenses data being sent to db now

--- lib/views/finance_page_views/bottom_sheets/add_expense_bottom_sheet_view.dart
+++ lib/views/finance_page_views/bottom_sheets/add_expense_bottom_sheet_view.dart
@@ -5,19 +5,25 @@ import 'package:shared_accommodation_management_app/view_models/group_view_model
 
 import '../../../view_models/finance_view_model.dart';
 
-class AddExpenseBottomSheetView extends StatelessWidget {
-
+class AddExpenseBottomSheetView extends StatefulWidget {
+  @override
+  State<AddExpenseBottomSheetView> createState() {
+    return _AddExpenseBottomSheetView();
+  }
+}
   User? user = FirebaseAuth.instance.currentUser;
+  final TextEditingController enteredExpenseNameController =
+      TextEditingController();
+  final TextEditingController enteredExpenseAmountController =
+      TextEditingController();
 
+  class _AddExpenseBottomSheetView extends State<AddExpenseBottomSheetView> {
   @override
   Widget build(BuildContext context) {
-    final TextEditingController enteredExpenseNameController = TextEditingController();
-
     return Consumer<FinanceViewModel>(builder: (context, viewModel, child) {
       return Padding(
           padding: EdgeInsets.only(
-              bottom: MediaQuery
-                  .of(context)
+              bottom: MediaQuery.of(context)
                   .viewInsets
                   .bottom), //Ensures the keyboard doesn't cover the textfields
           child: Container(
@@ -39,12 +45,11 @@ class AddExpenseBottomSheetView extends StatelessWidget {
                     //   Navigator.of(context).pop(); //Makes bottom expense bar disappear
                     // }
                   ),
-
                   TextField(
                     decoration: const InputDecoration(
                         hintText: "Expense Amount",
                         border: OutlineInputBorder()),
-                    controller: enteredExpenseNameController,
+                    controller: enteredExpenseAmountController,
                     // onSubmitted: (value) {
                     //   if (enteredExpenseNameController.text.isNotEmpty) {
                     //     Expense newExpense = Expense.newExpense(.text);
@@ -54,11 +59,12 @@ class AddExpenseBottomSheetView extends StatelessWidget {
                     //   Navigator.of(context).pop(); //Makes bottom expense bar disappear
                     // }
                   ),
-
                   IconButton(
-                    icon: Icon(Icons.add), onPressed: () {
-                    assignUsersToExpensePopup(context);
-                  },),
+                    icon: Icon(Icons.add),
+                    onPressed: () {
+                      assignUsersToExpensePopup(context);
+                    },
+                  ),
                 ],
               )));
     });
@@ -69,56 +75,76 @@ class AddExpenseBottomSheetView extends StatelessWidget {
     //
     // int membersSize = groupViewModel.returnGroupMembersSize(user!.uid) as int;
 
+    FinanceViewModel financeViewModel = FinanceViewModel();
+
+    List<String> assignedUsers = <String>[];
+
     showDialog(
         context: context,
         builder: (BuildContext context) {
           return Consumer<GroupViewModel>(
               builder: (BuildContext context, viewModel, child) {
-                return AlertDialog(
-                  scrollable: true,
-                  title: Text('Add Users'),
-                  content: SizedBox(
-                    width: double.maxFinite,
-                    height: 200,
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Form(
-                        child: Column(
-                          children: <Widget>[
-                            ListView.separated(
-                              shrinkWrap: true,
-                              separatorBuilder: (context, index) {
-                                return SizedBox(height: 15);
-                              },
-                              itemCount: viewModel.members.length,
-                              itemBuilder: (context, index) {
-                                return Container(
+            return AlertDialog(
+              scrollable: true,
+              title: Text('Add Users'),
+              content: SizedBox(
+                width: double.maxFinite,
+                height: 200,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Form(
+                    child: Column(
+                      children: <Widget>[
+                        ListView.separated(
+                            shrinkWrap: true,
+                            separatorBuilder: (context, index) {
+                              return SizedBox(height: 15);
+                            },
+                            itemCount: viewModel.members.length,
+                            itemBuilder: (context, index) {
+                              return GestureDetector(
+                                key: UniqueKey(),
+                                child: Container(
                                   decoration: BoxDecoration(
                                       color: viewModel.colour1,
                                       borderRadius: BorderRadius.circular(20)),
                                   child: ListTile(
-                                    title: Text(viewModel.members[index].toString(),
-                                        style:
-                                        TextStyle(color: viewModel.colour4, fontSize: 16)),
-                                  ),
-                                );
+                                      title: Text(
+                                          viewModel.members[index],
+                                          style: TextStyle(
+                                              color: viewModel.colour4,
+                                              fontSize: 16)),
+                                      onTap: () => {
+                                            assignedUsers.add(viewModel.members[index]),
 
-                              }
-                            )
-                          ],
-                        ),
-                      ),
+                                        setState(() {
+                                      viewModel.members.removeAt(index);
+                                        })
+                                          }),
+                                ),
+                              );
+                            })
+                      ],
                     ),
                   ),
-                  actions: [
-                    ElevatedButton(
-                        child: Text("Submit"),
-                        onPressed: () {
-                          // your code
-                        })
-                  ],
-                );
-              });
+                ),
+              ),
+              actions: [
+                ElevatedButton(
+                    child: Text("Submit"),
+                    onPressed: () {
+                      if (enteredExpenseNameController.text.isNotEmpty &&
+                          enteredExpenseAmountController.text.isNotEmpty) {
+                        financeViewModel.createExpense(
+                            user!.uid,
+                            enteredExpenseNameController.text,
+                            double.tryParse(enteredExpenseAmountController.text) ?? 0.0,
+                            assignedUsers);
+                      }
+                    })
+              ],
+            );
+          });
         });
   }
 }
